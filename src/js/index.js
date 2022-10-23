1bef46886fe804342a45257fa14b8c5c408150de
Hi-tei-hi-me <lethargium@gmail.com>
2022-10-23 20:33:02 +0300

    Basic update No.2

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import '../css/common.css';
+import '../css/search-form.css';
+import '../css/gallery.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import InfiniteScroll from 'infinite-scroll';
